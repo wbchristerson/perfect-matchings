a269fcd9f1959cfde397982d9fbeb1445436ab82
Will Christerson <wbchristerson@gmail.com>
2018-05-09 09:41:12 -0400

    refactor: Create inherited class for hoverable sprites

--- display.py
+++ display.py
@@ -3,21 +3,19 @@ from livewires import games, color
 # 640, 480
 games.init(screen_width = 832, screen_height = 624, fps = 50)
 
-class Vertex(games.Sprite):
-    def __init__(self, sprite_image, x_coor, y_coor):
-        super(Vertex, self).__init__(image = sprite_image, x = x_coor,
-                                     y = y_coor)
+class ClickableSprite(games.Sprite):
+    def __init__(self, sprite_image, x_coor, y_coor, new_hovered_image,
+                 new_selected_image):
+        super(ClickableSprite, self).__init__(sprite_image, x_coor, y_coor)
+        self.plain_image = sprite_image
+        self.hovered_image = new_hovered_image
+        self.selected_image = new_selected_image
         self.is_selected = False
         # set when to initiate the counter
         self.set_counter = False
         # count number of steps since a change in vertex color
         self.step_count = 0
-        self.id = 0
-        
-    pan = games.load_image("vertex.png")
-    hovered_pan = games.load_image("hovered-vertex.png")
-    selected_pan = games.load_image("selected-vertex.png")
-    
+
     def update(self):
         is_chosen = False
         if (self.set_counter and (self.step_count == 19)):
@@ -25,28 +23,73 @@ class Vertex(games.Sprite):
             self.set_counter = False
         elif (self.set_counter):
             self.step_count += 1
-
         for item in self.overlapping_sprites:
             if (item.id == 1):
                 is_chosen = True
                 break
-          
         if (is_chosen and (not self.is_selected) and
             (games.keyboard.is_pressed(games.K_SPACE)) and
             (not self.set_counter)):
             self.set_counter = True
-            self.image = self.selected_pan
+            self.image = self.selected_image
             self.is_selected = True
         elif (is_chosen and self.is_selected and
               (games.keyboard.is_pressed(games.K_SPACE)) and
               (not self.set_counter)):
             self.set_counter = True
-            self.image = self.pan
+            self.image = self.plain_image
             self.is_selected = False
         elif (is_chosen and (not self.is_selected)):
-            self.image = self.hovered_pan
+            self.image = self.hovered_image
         elif (not self.is_selected):
-            self.image = self.pan
+            self.image = self.plain_image
+
+
+class Vertex(ClickableSprite):
+    def __init__(self, sprite_image, x_coor, y_coor, new_hovered_image,
+                 new_selected_image):
+        super(Vertex, self).__init__(sprite_image, x_coor, y_coor,
+                                     new_hovered_image, new_selected_image)
+        #self.is_selected = False
+        # set when to initiate the counter
+        #self.set_counter = False
+        # count number of steps since a change in vertex color
+        #self.step_count = 0
+        self.id = 0
+        
+    #pan = games.load_image("vertex.png")
+    #hovered_pan = games.load_image("hovered-vertex.png")
+    #selected_pan = games.load_image("selected-vertex.png")
+    
+#    def update(self):
+#        is_chosen = False
+#        if (self.set_counter and (self.step_count == 19)):
+#            self.step_count = 0
+#            self.set_counter = False
+#        elif (self.set_counter):
+#            self.step_count += 1
+#
+#        for item in self.overlapping_sprites:
+#            if (item.id == 1):
+#                is_chosen = True
+#                break
+#          
+#        if (is_chosen and (not self.is_selected) and
+#            (games.keyboard.is_pressed(games.K_SPACE)) and
+#            (not self.set_counter)):
+#            self.set_counter = True
+#            self.image = self.selected_pan
+#            self.is_selected = True
+#        elif (is_chosen and self.is_selected and
+#              (games.keyboard.is_pressed(games.K_SPACE)) and
+#              (not self.set_counter)):
+#            self.set_counter = True
+#            self.image = self.pan
+#            self.is_selected = False
+#        elif (is_chosen and (not self.is_selected)):
+#            self.image = self.hovered_pan
+#        elif (not self.is_selected):
+#            self.image = self.pan
 
 
 # check mouse hovers by having an imageless sprite follow the mouse
@@ -68,23 +111,29 @@ class MyText(games.Text):
         self.id = 2
 
 # create a class for buttons
-class MyButton(games.Sprite):
-    def __init__(self, new_image, new_x, new_y):
-        super(MyButton, self).__init__(image = new_image, x = new_x, y = new_y)
+class MyButton(ClickableSprite):
+    def __init__(self, new_image, new_x, new_y, new_hovered_image,
+                 new_text = ''):
+        super(MyButton, self).__init__(new_image, new_x, new_y,
+                                       new_hovered_image, None)
         self.id = 3
+        self.text = new_text        
 
 def main():
     wall_image = games.load_image("wall-large.jpg", transparent = False)
     games.screen.background = wall_image
-    pizza_image = games.load_image("vertex.png")
     phantom_pizza_image = games.load_image("phantom-pizza.bmp")
+    vertex = games.load_image("vertex.png")
+    hovered_vertex = games.load_image("hovered-vertex.png")
+    selected_vertex = games.load_image("selected-vertex.png")
     pizza_list = []
     #for i in range(5):
     #    for j in range(2):
     #        pizza_list.append(Vertex(pizza_image, 200 + 240 * j, 80 * i + 80))
     for i in range(10):
         for j in range(2):
-            pizza_list.append(Vertex(pizza_image, 100 + 340 * j, 70 * i + 30))
+            pizza_list.append(Vertex(vertex, 100 + 340 * j, 70 * i + 30,
+                                     hovered_vertex, selected_vertex))
     pm = PhantomMouse(phantom_pizza_image)
     for pizza in pizza_list:
         games.screen.add(pizza)
@@ -93,10 +142,19 @@ def main():
     left_choice_text = MyText('Left branch size:', 30, color.black, 600, 30)
     games.screen.add(left_choice_text)
 
-    button_image = games.load_image("button-rectangle.png")
-    button = MyButton(button_image, 600, 200)
-    games.screen.add(button)
-    
+    button_image = games.load_image("button.png")
+    hovered_button_image = games.load_image("hovered-button.png")
+    button_list = []
+    text_list = []
+    for i in range(10):
+        button_list.append(MyButton(button_image, 600, 90 + 55 * i,
+                                    hovered_button_image, str(i+1)))
+        text_list.append(MyText(str(i+1), 20, color.black, 600, 90 + 55 * i))
+    for b in button_list:
+        games.screen.add(b)
+    for t in text_list:
+        games.screen.add(t)
+        
     games.screen.mainloop()
 
 main()
