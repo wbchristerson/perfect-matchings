c823e374724dc65f402c3d4d9b553d4d28f91101
Will Christerson <wbchristerson@gmail.com>
2018-05-10 07:52:27 -0400

    refactor: Organize code into class for entire application

--- display.py
+++ display.py
@@ -3,6 +3,7 @@ from livewires import games, color
 # 640, 480
 games.init(screen_width = 832, screen_height = 624, fps = 50)
 
+
 class ClickableSprite(games.Sprite):
     def __init__(self, sprite_image, x, y, hovered_image, selected_image):
         super(ClickableSprite, self).__init__(image = sprite_image, x = x,
@@ -50,46 +51,7 @@ class Vertex(ClickableSprite):
                  new_selected_image):
         super(Vertex, self).__init__(sprite_image, x_coor, y_coor,
                                      new_hovered_image, new_selected_image)
-        #self.is_selected = False
-        # set when to initiate the counter
-        #self.set_counter = False
-        # count number of steps since a change in vertex color
-        #self.step_count = 0
         self.id = 0
-        
-    #pan = games.load_image("vertex.png")
-    #hovered_pan = games.load_image("hovered-vertex.png")
-    #selected_pan = games.load_image("selected-vertex.png")
-    
-#    def update(self):
-#        is_chosen = False
-#        if (self.set_counter and (self.step_count == 19)):
-#            self.step_count = 0
-#            self.set_counter = False
-#        elif (self.set_counter):
-#            self.step_count += 1
-#
-#        for item in self.overlapping_sprites:
-#            if (item.id == 1):
-#                is_chosen = True
-#                break
-#          
-#        if (is_chosen and (not self.is_selected) and
-#            (games.keyboard.is_pressed(games.K_SPACE)) and
-#            (not self.set_counter)):
-#            self.set_counter = True
-#            self.image = self.selected_pan
-#            self.is_selected = True
-#        elif (is_chosen and self.is_selected and
-#              (games.keyboard.is_pressed(games.K_SPACE)) and
-#              (not self.set_counter)):
-#            self.set_counter = True
-#            self.image = self.pan
-#            self.is_selected = False
-#        elif (is_chosen and (not self.is_selected)):
-#            self.image = self.hovered_pan
-#        elif (not self.is_selected):
-#            self.image = self.pan
 
 
 # check mouse hovers by having an imageless sprite follow the mouse
@@ -110,6 +72,7 @@ class MyText(games.Text):
                                      color = new_color, x = new_x, y = new_y)
         self.id = 2
 
+
 # create a class for buttons
 class MyButton(ClickableSprite):
     def __init__(self, new_image, new_x, new_y, new_hovered_image,
@@ -119,59 +82,105 @@ class MyButton(ClickableSprite):
         self.id = 3
         self.text = new_text
 
-# class to monitor changes in input
-class Responses(games.Sprite):
-    def __init__(self, screen, sprite_image):
-        super(Responses, self).__init__(image = sprite_image, x = 0, y = 0)
-        self.screen = screen
+
+# class to monitor changes in input; this class is loosely based on the 'Game'
+# class of the final astrocrash game described on page 402 of 'Python
+# Programming for the Absolute Beginner, Third Edition', by Michael Dawson,
+# Chapter 12: Sound, Animation, and Program Development: The Astrocrash Game
+class Responses(object):
+    def __init__(self):
         self.state = 0
+        self.left_size = 0
+        self.right_size = 0
+        self.left_branch = [] # list of vertices of left branch
+        self.right_branch = [] # list of vertices of right branch
+        self.button_list = [] # list of buttons as ClickableSprites
+        self.main_text_sprite = None # Text sprite for main content
+        self.text_list = [] # list of text sprites appearing on buttons
+
+    def set_left_branch_query(self):
+        self.main_text_sprite = MyText('Left branch size:', 30, color.black,
+                                       600, 30)
+        games.screen.add(self.main_text_sprite)
+        button_image = games.load_image("button.png")
+        hovered_button_image = games.load_image("hovered-button.png")
+        selected_button_image = games.load_image("selected-button.png")
+
+        for i in range(5):
+            self.button_list.append(MyButton(button_image, 550, 90 + 55 * i,
+                                             hovered_button_image,
+                                             selected_button_image, str(i+1)))
+            self.text_list.append(MyText(str(i+1), 20, color.black, 550,
+                                         90 + 55 * i))
+        for i in range(5):
+            self.button_list.append(MyButton(button_image, 700, 90 + 55 * i,
+                                             hovered_button_image,
+                                             selected_button_image, str(i+6)))
+            self.text_list.append(MyText(str(i+6), 20, color.black, 700,
+                                         90 + 55 * i))
+        for b in self.button_list:
+            games.screen.add(b)
+        for t in self.text_list:
+            games.screen.add(t)
+        #vertex = games.load_image("vertex.png")
+        #hovered_vertex = games.load_image("hovered-vertex.png")
+        #selected_vertex = games.load_image("selected-vertex.png")
+        #for i in range(10):
+        #    for j in range(2):
+        #        self.button_list.append(Vertex(vertex, 100 + 340 * j,
+        #                                       70 * i + 30, hovered_vertex,
+        #                                       selected_vertex))
+
+    def advance(self):
+        self.state += 1
+        if (self.state == 1):
+            self.set_left_branch_query()
+
+    def play(self):
+        wall_image = games.load_image("wall-large.jpg")
+        games.screen.background = wall_image
+        self.advance()
+        games.screen.mainloop()
+
 
 def main():
-    wall_image = games.load_image("wall-large.jpg", transparent = False)
-    games.screen.background = wall_image
+    #wall_image = games.load_image("wall-large.jpg", transparent = False)
+    #games.screen.background = wall_image
     phantom_pizza_image = games.load_image("phantom-pizza.bmp")
-    vertex = games.load_image("vertex.png")
-    hovered_vertex = games.load_image("hovered-vertex.png")
-    selected_vertex = games.load_image("selected-vertex.png")
-    pizza_list = []
-    #for i in range(5):
-    #    for j in range(2):
-    #        pizza_list.append(Vertex(pizza_image, 200 + 240 * j, 80 * i + 80))
-    for i in range(10):
-        for j in range(2):
-            pizza_list.append(Vertex(vertex, 100 + 340 * j, 70 * i + 30,
-                                     hovered_vertex, selected_vertex))
-    pm = PhantomMouse(phantom_pizza_image)
-
-    # class object to control responses to buttons
-    controller = Responses(games.screen, phantom_pizza_image)
     
-    for pizza in pizza_list:
-        games.screen.add(pizza)
+    pm = PhantomMouse(phantom_pizza_image)
     games.screen.add(pm)
 
-    left_choice_text = MyText('Left branch size:', 30, color.black, 600, 30)
-    games.screen.add(left_choice_text)
-    button_image = games.load_image("button.png")
-    hovered_button_image = games.load_image("hovered-button.png")
-    selected_button_image = games.load_image("selected-button.png")
-    button_list = []
-    text_list = []
-    for i in range(5):
-        button_list.append(MyButton(button_image, 550, 90 + 55 * i,
-                                    hovered_button_image, selected_button_image,
-                                    str(i+1)))
-        text_list.append(MyText(str(i+1), 20, color.black, 550, 90 + 55 * i))
-    for i in range(5):
-        button_list.append(MyButton(button_image, 700, 90 + 55 * i,
-                                    hovered_button_image, selected_button_image,
-                                    str(i+6)))
-        text_list.append(MyText(str(i+6), 20, color.black, 700, 90 + 55 * i))
-    for b in button_list:
-        games.screen.add(b)
-    for t in text_list:
-        games.screen.add(t)
+    # class object to control responses to buttons
+    controller = Responses()
+    controller.play()
+    
+    #for pizza in pizza_list:
+    #    games.screen.add(pizza)
+
+    #left_choice_text = MyText('Left branch size:', 30, color.black, 600, 30)
+    #games.screen.add(left_choice_text)
+    #button_image = games.load_image("button.png")
+    #hovered_button_image = games.load_image("hovered-button.png")
+    #selected_button_image = games.load_image("selected-button.png")
+    #button_list = []
+    #text_list = []
+
+    #for i in range(5):
+    #    button_list.append(MyButton(button_image, 550, 90 + 55 * i,
+    #                                hovered_button_image, selected_button_image,
+    #                                str(i+1)))
+    #    text_list.append(MyText(str(i+1), 20, color.black, 550, 90 + 55 * i))
+    #for i in range(5):
+    #    button_list.append(MyButton(button_image, 700, 90 + 55 * i,
+    #                                hovered_button_image, selected_button_image,
+    #                                str(i+6)))
+    #    text_list.append(MyText(str(i+6), 20, color.black, 700, 90 + 55 * i))
+    #for b in button_list:
+    #    games.screen.add(b)
+    #for t in text_list:
+    #    games.screen.add(t)
         
-    games.screen.mainloop()
+    #games.screen.mainloop()
 
 main()
