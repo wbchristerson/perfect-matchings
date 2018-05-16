5df24e12a5da1f02e7f46d5b3b735af7141df20d
Will Christerson <wbchristerson@gmail.com>
2018-05-16 10:08:43 -0400

    feat: Implement partial display for initial greedy matching

--- AlgorithmDisplay.py
+++ AlgorithmDisplay.py
@@ -14,6 +14,10 @@ class AlgorithmDisplay(games.Sprite):
         self.ticker = 0
         self.left_index = 0
         self.right_index = 0
+        self.in_greedy_stage = True # whether finding initial greedy matching
+        self.has_highlighted_edge = False # whether an edge is being examined
+        self.highlighted_edge = None # currently examined edge
+        self.matching = []
 
     def get_edge(self, left_vertex, right_vertex):
         for edge in self.responder.edges:
@@ -28,7 +32,11 @@ class AlgorithmDisplay(games.Sprite):
             if (self.ticker == 100):
                 self.ticker = 0
                 self.is_counting = False
-        else:
+                
+        elif (self.left_index == self.left_size):
+            self.in_greedy_stage = False
+            
+        elif ((self.in_greedy_stage) and (not self.has_highlighted_edge)):
             while ((self.left_index < self.left_size) and
                    (len(self.left_neighbors[self.left_index]) == 0)):
                 self.left_index += 1
@@ -39,10 +47,24 @@ class AlgorithmDisplay(games.Sprite):
                 #print(self.left_neighbors[self.right_index])
                 edge.set_image(edge.hovered_image)
                 self.is_counting = True
+                self.has_highlighted_edge = True
+                self.highlighted_edge = edge
+
+        elif ((self.in_greedy_stage) and self.has_highlighted_edge):
+            if (GA.right_is_already_matched(self.highlighted_edge.right_vertex,
+                                            self.matching)):
+                self.highlighted_edge.set_image(self.highlighted_edge.edge_image)
+                self.highlighted_edge = None
+                self.has_highlighted_edge = False
                 if (self.right_index ==
-                    (len(self.left_neighbors[self.left_index]) -1)):
+                    (len(self.left_neighbors[self.left_index]) - 1)):
                     self.left_index += 1
                     self.right_index = 0
                 else:
                     self.right_index += 1
-                
+            else:
+                self.highlighted_edge.set_image(self.highlighted_edge.selected_image)
+                self.matching.append((self.left_index,
+                                      self.left_neighbors[self.left_index][self.right_index]))
+                self.left_index += 1
+                self.right_index = 0
